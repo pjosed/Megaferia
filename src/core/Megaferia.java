c4c1f42df9eab497976cf5be92e328d293bc88c3
pjosed <pjosed@uninorte.edu.co>
2024-09-18 14:25:52 -0500

    Clases añadidas

---
+++ src/core/Megaferia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pjosed
+ */
+public class Megaferia {
+    private ArrayList<Stand> stands;
+    private ArrayList<Editorial> editoriales;
+}
---
+++ src/core/Stand.java
@@ -0,0 +1,6 @@
+package core;
+public class Stand {
+   private int id;
+   private float precio;
+   private ArraList<Editorial> editoriales; 
+}
